d3693941098163351bd8f08111850de39df23b5c
kalebritt <68469962+kalebritt@users.noreply.github.com>
2022-12-17 17:21:23 -0700

    auth.js work

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -2,3 +2,28 @@ const jwt = require("jsonwebtoken");
 
 const secret = "mysecretssshhhhhhh";
 const expiration = "2h";
+
+//add them module.exports
+module.exports = {
+  authMiddleware: function ({ req }) {
+    let token = req.body.token || req.query.token || req.headers.authorization;
+
+    if (req.headers.authorization) {
+      token = token.split(" ").pop().trim();
+    }
+
+    if (!token) {
+      return req;
+    }
+
+    try {
+      const { data } = jwt.verify(token, secret, { maxAge: expiration });
+      req.user = data;
+    } catch {
+      console.log("Invalid token");
+    }
+
+    return req;
+  },
+  //signToken: function
+};
